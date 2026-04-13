008be757fad897ab988762c90eff3ea7f5a1703e
Hugh McCredie <mccredih@oregonstate.edu>
2026-04-12 20:48:31 -0700

    Made the search bar functional using a steams store Api

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+let allGames = [];
+
+async function searchGames(query) {
+    const response = await fetch(`https://corsproxy.io/?url=${encodeURIComponent('https://store.steampowered.com/api/storesearch/?term=' + query + '&l=english&cc=US')}`);
+    const data = await response.json();
+    console.log(data);
+}
+
+const searchInput = document.querySelector('.search-input');
+
+searchInput.addEventListener('input', (e) => {
+    const query = e.target.value;
+    if (query.length >= 3) {
+        searchGames(query);
+    }
+});
